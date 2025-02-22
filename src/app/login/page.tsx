852cb9f9dd8f20d8c79b561a4add024c40ae677f
guredora <contact@guredora.com>
2025-02-22 14:40:25 +0900

    ログイン画面で、useStateの初期化関数を利用するように変更

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,32 +1,27 @@
 "use client"
 import { createClient } from "~/utils/supabase/client"
 import { Auth } from "@supabase/auth-ui-react"
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { env } from "~/env"
 import { api } from "~/trpc/react"
 import { useRouter } from "next/navigation"
+import { getBaseUrl } from "~/utils/url"
 
 export default function Page() {
-    const [supabase, setSupabase] = useState<ReturnType<typeof createClient> | null>(null)
-    const [redirectUrl, setRedirectUrl] = useState<string>("")
     const router = useRouter();
-    const {data: isDev} = api.isDev.useQuery();
-    useEffect(() => {
+    const [supabase] = useState<ReturnType<typeof createClient>>(() => {
         const _client = createClient()
         _client.auth.onAuthStateChange((event, session) => {
             if(event === "SIGNED_IN"){
                 router.push("/")
                 router.refresh()
             }
-            const baseUrl = window.location.origin
-            const redirectUrl = `${baseUrl}/auth/callback`
-            setRedirectUrl(redirectUrl)
         })
-        setSupabase(_client)
-        return () => {}
-    }, [])
+        return _client
+    })
+    const {data: isDev} = api.isDev.useQuery();
     return <div>
         <h1>Supabase Auth</h1>
-        {supabase && <Auth supabaseClient={supabase} providers={["github", "discord"]} onlyThirdPartyProviders={!isDev} redirectTo={redirectUrl}/>}
+        <Auth supabaseClient={supabase} providers={["github", "discord"]} onlyThirdPartyProviders={!isDev} redirectTo={`${getBaseUrl()}/auth/callback`}/>
     </div>
 }
