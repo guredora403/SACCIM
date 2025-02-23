79b21350f59159542a9c4f903b6a234a619166f3
guredora <contact@guredora.com>
2025-02-24 00:32:35 +0900

    アバター一覧画面

---
+++ src/app/_components/ListAvatars.tsx
@@ -0,0 +1,22 @@
+// listing the avatars
+import { ActionMenu, Item } from "@adobe/react-spectrum";
+import { api } from "~/trpc/react";
+
+export function ListAvatars(){
+    const [avatars] = api.avatar.getAll.useSuspenseQuery();
+    if(avatars.length === 0){
+        return <div>アバターがありません。</div>
+    }
+    return <div>
+        {avatars.map(avatar => {
+            return <div key={avatar.id}>
+                <p>{avatar.name}</p>
+                <ActionMenu>
+                    <Item key="detail">詳細</Item>
+                    <Item key="invite">招待</Item>
+                    <Item key="delete">削除</Item>
+                </ActionMenu>
+            </div>
+        })}
+    </div>
+}
--- src/app/avatars/page.tsx
+++ src/app/avatars/page.tsx
@@ -2,16 +2,18 @@
 import { ActionButton, DialogTrigger, Heading, View } from "@adobe/react-spectrum";
 import { CreateAvatarDialog } from "../_components/CreateAvatarDialog";
 import { api } from "~/trpc/react";
+import { ListAvatars } from "../_components/ListAvatars";
 
 
 export default function Avatars() {
-    return <View>
-        <Heading level={2}>アバター一覧</Heading>
+    return <div>
+        <h2>アバター一覧</h2>
         <DialogTrigger type="modal">
             <ActionButton>アバターを新規作成</ActionButton>
             {(close) => (
                 <CreateAvatarDialog close = {close}/>
             )}
         </DialogTrigger>
-    </View>
+        <ListAvatars/>
+    </div>
 }
