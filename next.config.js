e76041697dfe80b85b9e2206f750579f20bc6b8a
guredora <contact@guredora.com>
2025-02-23 14:48:00 +0900

    react-spectrum 導入準備

--- next.config.js
+++ next.config.js
@@ -4,8 +4,16 @@
  */
 import "./src/env.js";
 /** @type {import('next').NextConfig} */
+import { glob} from "glob"
 
 const config = {
+    transpilePackages: [
+        '@adobe/react-spectrum',
+        '@react-spectrum/*',
+        '@spectrum-icons/*'
+    ].flatMap((spec) =>
+        glob.sync(`${spec}`, { cwd: 'node_modules/' })
+    )
 };
 
 export default config;
