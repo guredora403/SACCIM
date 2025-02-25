c0eda1e0dd01ac1133a38f93e09d90b611f0063d
guredora <contact@guredora.com>
2025-02-26 03:31:00 +0900

    use ssr and hydrate tRPC for bug

--- src/app/avatars/page.tsx
+++ src/app/avatars/page.tsx
@@ -1,19 +1,10 @@
-"use client";
-import { ActionButton, DialogTrigger, Heading, View } from "@adobe/react-spectrum";
-import { CreateAvatarDialog } from "../_components/CreateAvatarDialog";
-import { api } from "~/trpc/react";
-import { ListAvatars } from "../_components/ListAvatars";
-
+import AvatarPage  from "~/app/_components/AvatarPage"
+import { api, HydrateClient } from "~/trpc/server"
 
 export default function Avatars() {
-    return <div>
-        <h2>アバター一覧</h2>
-        <DialogTrigger type="modal">
-            <ActionButton>アバターを新規作成</ActionButton>
-            {(close) => (
-                <CreateAvatarDialog close = {close}/>
-            )}
-        </DialogTrigger>
-        <ListAvatars/>
-    </div>
+    void api.avatar.getAll.prefetch()
+
+    return <HydrateClient>
+        <AvatarPage/>
+    </HydrateClient>
 }
