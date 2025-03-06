376561dcebbd913ab189eacc2b8e3fc9f30f91c4
guredora <contact@guredora.com>
2025-03-06 23:02:41 +0900

    ログインリンクコンポーネント改善

--- src/app/_components/LoginLink.tsx
+++ src/app/_components/LoginLink.tsx
@@ -3,16 +3,18 @@ import { useState, useEffect } from "react";
 import { Link } from "@adobe/react-spectrum";
 
 export function LoginLink() {
-  const [currentUrl, setCurrentUrl] = useState<string>("");
+  const [currentPath, setCurrentPath] = useState<string>("");
   useEffect(() => {
     if (typeof window !== "undefined") {
-      if (!window.location.href.endsWith("/login")) {
-        setCurrentUrl(window.location.href);
+      if (!window.location.pathname.endsWith("/login")) {
+        setCurrentPath(window.location.pathname);
       }
     }
   }, []);
 
   return (
-    <Link href={`/login?next=${encodeURIComponent(currentUrl)}`}>ログイン</Link>
+    <Link href={`/login?next=${encodeURIComponent(currentPath)}`}>
+      ログイン
+    </Link>
   );
 }
