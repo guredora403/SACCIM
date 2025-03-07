bba6f2880d7cfeb7f993a2c4b16b9fb1715c4f99
guredora <contact@guredora.com>
2025-03-08 03:40:46 +0900

    動的レンダリングを強制するための設定を追加

--- src/app/mycontact/page.tsx
+++ src/app/mycontact/page.tsx
@@ -1,6 +1,7 @@
 import { MyContactPage } from "../_components/mycontact";
 import { api, HydrateClient } from "~/trpc/server";
 
+export const dynamic = "force-dynamic";
 export default function MyContact() {
   void api.myContact.getAll.prefetch();
   return (
