04c4813e57c097fc32a86fd24237326f32eccf8d
guredora <contact@guredora.com>
2025-04-10 12:16:59 +0900

    vercel optimizing

--- src/env.js
+++ src/env.js
@@ -7,7 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url(),
+    POSTGRES_PRISMA_URL: z.string().url(),
     SITE_URL: z.string().url().nullish(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
@@ -30,7 +30,7 @@ export const env = createEnv({
    * middlewares) or client-side so we need to destruct manually.
    */
   runtimeEnv: {
-    DATABASE_URL: process.env.DATABASE_URL,
+    POSTGRES_PRISMA_URL: process.env.POSTGRES_PRISMA_URL,
     SITE_URL: process.env.SITE_URL,
     NODE_ENV: process.env.NODE_ENV,
     // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
